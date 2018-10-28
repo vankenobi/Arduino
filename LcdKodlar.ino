97a6d006837bcda72f706f4bcdf04c7f0587eec0
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-10-28 15:39:04 +0300

    Create LcdKodlar.ino

---
+++ LcdKodlar.ino
@@ -0,0 +1,7 @@
+/*
+lcd.begin() LCD ekranınızın boyutlarını girmek için kullandığımız komut
+lcd.setCursor() yazı yazmak için imleci istediğimiz yere koymamızı sağlayan komut
+lcd.print() ekrana yazdırmak istediğimiz şeyleri bu komutla yazdıracağız
+lcd.clear() ekrandaki her şeyi temizleyen komut
+lcd.home() imleci (0,0) yani ekranın en üst en sol köşesine başa taşıyan komut
+*/
